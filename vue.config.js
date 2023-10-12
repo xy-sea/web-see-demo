f58d89458c649596f07baaeec002931bfa45e0fe
海阔天空 <xy_sea@163.com>
2023-10-12 11:43:26 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ const { PerfseePlugin } = require('@perfsee/webpack')
 // const prodDir = 'web-see-demo';
 
 module.exports = defineConfig({
-  publicPath: './',
+  publicPath: 'dist',
   // outputDir: `${prodDir}`,
   devServer: {
     proxy: {
